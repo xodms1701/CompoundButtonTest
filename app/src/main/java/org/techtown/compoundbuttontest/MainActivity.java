999b57191559e23ca0b3ef08bf1c82c5f70c48d3
xodms1701 <kimrrrrt123@naver.com>
2017-03-28 20:55:44 +0900

    #1 xml layout

---
+++ app/src/main/java/org/techtown/compoundbuttontest/MainActivity.java
@@ -0,0 +1,13 @@
+package org.techtown.compoundbuttontest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
